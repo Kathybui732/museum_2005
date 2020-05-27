a5b88829aba370c4d90fc3e5b5c177a82ddb54cf
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-27 09:30:37 -0600

    Create method for recommend exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,13 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  # have to take theinstance object and add it instead of name to array
+  def recommend_exhibits(patron)
+    recommended = []
+    @exhibits.each do |exhibit|
+      recommended << exhibit if patron.interests.include?(exhibit.name)
+    end
+    recommended
+  end
 end
