54668e0c3fd7ceb591ce384cf905f7afdd4bbffc
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-27 09:37:08 -0600

    Create method for admit and patrons

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,10 +1,12 @@
 class Museum
   attr_reader :name,
-              :exhibits
+              :exhibits,
+              :patrons
 
   def initialize(name)
     @name = name
     @exhibits = []
+    @patrons = []
   end
 
   def add_exhibit(exhibit)
@@ -19,4 +21,12 @@ class Museum
     end
     recommended
   end
+
+  def admit(patron)
+    @patrons << patron
+  end
+
+  # For patrons_by_exhibit_interest, this method takes no arguments and returns a Hash where each key is an Exhibit. The value associated with that Exhibit is an Array of all the Patrons that have an interest in that exhibit.
+  # ticket_lottery_contestants returns an array of patrons that do not have enough money to see an exhibit, but are interested in that exhibit. The lottery winner is generated randomly based on the available contestants when draw_lottery_winner is called.
+  # You will need to use a stub to test the announce_lottery_winner method in conjunction with the draw_lottery_winner method. JOY!
 end
