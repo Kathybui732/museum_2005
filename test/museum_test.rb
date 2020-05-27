4c0421e182c4a5e2044b366a13dc384efffb35ae
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-27 09:49:25 -0600

    Add test for draw lottery winner

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -57,4 +57,55 @@ class MuseumTest < MiniTest::Test
     @dmns.admit(@patron_3)
     assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
   end
+
+  def test_patrons_by_exhibit_interests
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    expected = {
+      @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3],
+      @gems_and_minerals => [@patron_1],
+      @imax => []
+    }
+    assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
+
+  def test_ticket_lottery_contestants
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    assert_equal [@patron_1, @patron_3], ticket_lottery_contestants(@dead_sea_scrolls)
+  end
+
+  def test_draw_lottery_winner
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal "Bob", @dmns.draw_lottery_winner(@dead_sea_scrolls)
+    assert_nil nil, @dmns.draw_lottery_winner(@gems_and_minerals)
+  end
+
+  
 end
