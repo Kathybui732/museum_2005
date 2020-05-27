f1d276a220d03b5d3d009804730e8e085e8b418c
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-27 10:05:22 -0600

    add patrons by interest method

--- lib/museum.rb
+++ lib/museum.rb
@@ -26,7 +26,22 @@ class Museum
     @patrons << patron
   end
 
-  # For patrons_by_exhibit_interest, this method takes no arguments and returns a Hash where each key is an Exhibit. The value associated with that Exhibit is an Array of all the Patrons that have an interest in that exhibit.
+  def patrons_by(exhibit)
+    @patrons.select do |patron|
+      patron.interests.include?(exhibit.name)
+    end
+  end
+
+  def patrons_by_exhibit_interest
+    pbei = Hash.new
+    @exhibits.each do |exhibit|
+      pbei[exhibit] = patrons_by(exhibit)
+    end
+    pbei
+  end
+
+
+
   # ticket_lottery_contestants returns an array of patrons that do not have enough money to see an exhibit, but are interested in that exhibit. The lottery winner is generated randomly based on the available contestants when draw_lottery_winner is called.
   # You will need to use a stub to test the announce_lottery_winner method in conjunction with the draw_lottery_winner method. JOY!
 end
