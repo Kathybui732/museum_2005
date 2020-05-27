6ffe5d12ab1b550e5e0b5df0ab742d183bb88f59
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-27 09:35:49 -0600

    Add test for admit patron

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -13,6 +13,7 @@ class MuseumTest < MiniTest::Test
     @imax = Exhibit.new({name: "IMAX",cost: 15})
     @patron_1 = Patron.new("Bob", 20)
     @patron_2 = Patron.new("Sally", 20)
+    @patron_3 = Patron.new("Johnny", 5)
   end
 
 	def test_it_exists
@@ -40,5 +41,20 @@ class MuseumTest < MiniTest::Test
     @patron_2.add_interest("IMAX")
     assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits(@patron_1)
     assert_equal [@imax], @dmns.recommend_exhibits(@patron_2)
+  end
+
+  def test_can_admit_patrons
+    assert_equal [], @dmns.patrons
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@imax)
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+    assert_equal [@patron_1, @patron_2, @patron_3], @dmns.patrons
   end
 end
